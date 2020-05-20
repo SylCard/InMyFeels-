fc7e578a13ab43e292c60460ffb243958e219cec
Sylvester <sbcardorelle@hotmail.co.uk>
2020-05-20 12:13:03 +0100

    Updates

---
+++ precache-manifest.269d34baaddb0c60d970495a1093b2e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93c5097d82c1f2c79def420a3876ab6",
+    "url": "/InMyFeels-/index.html"
+  },
+  {
+    "revision": "e541a867573ac5abf5ec",
+    "url": "/InMyFeels-/static/css/main.50f54316.chunk.css"
+  },
+  {
+    "revision": "3f6ea1e61e15db5ef49a",
+    "url": "/InMyFeels-/static/js/2.053dd012.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/InMyFeels-/static/js/2.053dd012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e541a867573ac5abf5ec",
+    "url": "/InMyFeels-/static/js/main.bcfee808.chunk.js"
+  },
+  {
+    "revision": "958d70f1433b807deab7",
+    "url": "/InMyFeels-/static/js/runtime-main.92623e8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/InMyFeels-/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SylCard/InMyFeels-/precache-manifest.6fc94902ac662216197842e30b2441b0.js"
+  "/InMyFeels-/precache-manifest.269d34baaddb0c60d970495a1093b2e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SylCard/InMyFeels-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/InMyFeels-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
